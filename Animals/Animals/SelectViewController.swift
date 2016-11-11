d4d2f6582e1b3de702156b8da2b638b0b5b5c5a8
Max Luttrell <mluttrell@ccsf.edu>
2016-11-11 09:55:43 -0800

    add animals demo from unit 12

---
+++ Animals/Animals/SelectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SelectViewController.swift
+//  Animals
+//
+//  Created by Max Luttrell on 11/3/16.
+//  Copyright © 2016 Max Luttrell. All rights reserved.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+        
+}
+
