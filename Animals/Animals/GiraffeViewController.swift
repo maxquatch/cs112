1dafee4de1c6c233655167e2c2faf1cc6af8a604
Max Luttrell <mluttrell@ccsf.edu>
2017-04-13 11:21:26 -0700

    Add unit 12 animal app demo

---
+++ Animals/Animals/GiraffeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GiraffeViewController.swift
+//  Animals
+//
+//  Created by Max Luttrell on 11/3/16.
+//  Copyright © 2016 Max Luttrell. All rights reserved.
+//
+
+import UIKit
+
+class GiraffeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
