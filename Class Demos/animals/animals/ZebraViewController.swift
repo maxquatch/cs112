a285e0e703ce82d5ab5f49717bac61ee4fbdd53f
Max Luttrell <mluttrell@ccsf.edu>
2018-04-16 10:12:07 -0700

    add animals demo

---
+++ Class Demos/animals/animals/ZebraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZebraViewController.swift
+//  animals
+//
+//  Created by Max Luttrell on 4/12/18.
+//  Copyright © 2018 Max Luttrell. All rights reserved.
+//
+
+import UIKit
+
+class ZebraViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        print("zebra back button tapped")
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
