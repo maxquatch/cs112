335b2c7b0aba81c4acdd8efc5b7c5b6f9756557f
Max Luttrell <mluttrell@ccsf.edu>
2018-09-27 09:57:22 -0700

    add GoGiants app

---
+++ GoGiants/GoGiants/ViewControllerGo.swift
@@ -0,0 +1,19 @@
+//
+//  ViewControllerGo.swift
+//  GoGiants
+//
+//  Created by Max Luttrell on 9/27/16.
+//  Copyright © 2016 Max Luttrell. All rights reserved.
+//
+//  View controller for "Go"
+
+import UIKit
+
+class ViewControllerGo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
