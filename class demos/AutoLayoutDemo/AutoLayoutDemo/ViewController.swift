377159a1e586efa484dd9ecd1f7ba6d3fb711719
Max Luttrell <mluttrell@ccsf.edu>
2019-05-07 13:09:11 -0700

    add auto layout demo

---
+++ class demos/AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Maximilian Luttrell on 5/7/19.
+//  Copyright © 2019 Maximilian Luttrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
